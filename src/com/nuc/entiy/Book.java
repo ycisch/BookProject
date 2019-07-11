df887ec4289b634b0295b1dea1ec0944f74af380
Zhi132 <837889599@qq.com>
2019-07-11 15:13:58 +0800

    test

--- src/com/nuc/entiy/Book.java
+++ src/com/nuc/entiy/Book.java
@@ -1,4 +1,67 @@
 package com.nuc.entiy;
 
 public class Book {
+    private int bookid;
+    private String bookName;
+    private String bookAuthor;
+    private String info;
+    private int stock;
+    private int styleId;
+    private int style;
+
+    public int getBookid() {
+        return bookid;
+    }
+
+    public void setBookid(int bookid) {
+        this.bookid = bookid;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getBookAuthor() {
+        return bookAuthor;
+    }
+
+    public void setBookAuthor(String bookAuthor) {
+        this.bookAuthor = bookAuthor;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public int getStyleId() {
+        return styleId;
+    }
+
+    public void setStyleId(int styleId) {
+        this.styleId = styleId;
+    }
+
+    public int getStyle() {
+        return style;
+    }
+
+    public void setStyle(int style) {
+        this.style = style;
+    }
 }
