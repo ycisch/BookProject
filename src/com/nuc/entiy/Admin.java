80c4a9ce1af9c733fcde4213d76b6ac99e9365a0
Zhi132 <837889599@qq.com>
2019-07-11 15:13:33 +0800

    test

--- src/com/nuc/entiy/Admin.java
+++ src/com/nuc/entiy/Admin.java
@@ -1,4 +1,31 @@
 package com.nuc.entiy;
 
 public class Admin {
+    private int adminId;
+    private String adminName;
+    private String adminPwd;
+
+    public int getAdminId() {
+        return adminId;
+    }
+
+    public void setAdminId(int adminId) {
+        this.adminId = adminId;
+    }
+
+    public String getAdminName() {
+        return adminName;
+    }
+
+    public void setAdminName(String adminName) {
+        this.adminName = adminName;
+    }
+
+    public String getAdminPwd() {
+        return adminPwd;
+    }
+
+    public void setAdminPwd(String adminPwd) {
+        this.adminPwd = adminPwd;
+    }
 }
