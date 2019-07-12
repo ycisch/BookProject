272002f64be62d718de1e7ead2a9760e3104b58d
yinchen_ <837889599@qq.com>
2019-07-12 11:32:25 +0800

    添加事务回滚 去除自动提交 添加文件上传 添加密码加密功能 添加分页功能

--- src/com/nuc/servlet/ShopServlet.java
+++ src/com/nuc/servlet/ShopServlet.java
@@ -1,14 +1,27 @@
 package com.nuc.servlet;
 
+import com.nuc.entiy.Shop;
+import com.nuc.entiy.User;
+import com.nuc.service.ShopService;
+import com.nuc.service.impl.ShopServiceImpl;
+
 import java.io.IOException;
+import java.util.List;
 
 public class ShopServlet extends javax.servlet.http.HttpServlet {
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
         request.setCharacterEncoding("UTF-8");
-        String opr = request.getParameter("opr");
-        if(opr == "list"){
 
+        String opr = request.getParameter("opr");
+
+        ShopService shopService = new ShopServiceImpl();
+        User user = (User) request.getSession().getAttribute("user");
+        Shop shop = new Shop();
+        if(opr == "list"){
+            List<Shop> list = shopService.listShop(user);
+            request.setAttribute("list",list);
         }else if(opr == "add"){
+
 
         }else if(opr == "delete"){
 
@@ -19,7 +32,7 @@ public class ShopServlet extends javax.servlet.http.HttpServlet {
         }
     }
 /*
-* wawawawawawqweqwewqe5956592562
+* wawawawawawqweqwewqe5956592562123213213asdasdasdsadsads
 *
 * */
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
