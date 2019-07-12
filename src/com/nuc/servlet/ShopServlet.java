884029af2261536400d330711e3dd32a3f7e4a6e
yinchen_ <837889599@qq.com>
2019-07-12 11:07:05 +0800

    添加事务回滚 去除自动提交 添加文件上传 添加密码加密功能 添加分页功能

--- src/com/nuc/servlet/ShopServlet.java
+++ src/com/nuc/servlet/ShopServlet.java
@@ -4,7 +4,19 @@ import java.io.IOException;
 
 public class ShopServlet extends javax.servlet.http.HttpServlet {
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        String opr = request.getParameter("opr");
+        if(opr == "list"){
 
+        }else if(opr == "add"){
+
+        }else if(opr == "delete"){
+
+        }else if(opr == "update"){
+
+        }else if(opr == "sum"){
+
+        }
     }
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
---
+++ src/com/nuc/util/Page.java
@@ -0,0 +1,59 @@
+package com.nuc.util;
+
+
+import java.util.List;
+
+public class Page {
+    //总页数
+    private  int totalPageCout=0;
+    //页面大小，即每页显示记录数
+    private int pageSize=5;
+    //总记录数
+    private int totalCount=0;
+    //当前页码
+    private int currPageNo=1;
+
+    public int getTotalPageCout() {
+        return totalPageCout;
+    }
+
+    public void setTotalPageCout(int totalPageCout) {
+        this.totalPageCout = totalPageCout;
+    }
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(int pageSize) {
+        if (pageSize>0) {
+            this.pageSize = pageSize;
+        }
+    }
+
+    public int getTotalCount() {
+        return totalCount;
+    }
+
+    public void setTotalCount(int totalCount) {
+        if (totalCount>0){
+            this.totalCount = totalCount;
+            //计算总页数
+            totalPageCout=this.totalCount%pageSize==0?(this.totalCount/pageSize):(this.totalCount/pageSize+1);
+        }
+
+    }
+
+    public int getCurrPageNo() {
+        if (totalPageCout==0){
+            return  0;
+        }
+        return currPageNo;
+    }
+
+    public void setCurrPageNo(int currPageNo) {
+        if (currPageNo>0) {
+            this.currPageNo = currPageNo;
+        }
+    }
+}
